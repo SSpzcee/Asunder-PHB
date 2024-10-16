4dc505d8b8937818b35c96bcd3066c0fd1e53470
SSpzcee <108297899+SSpzcee@users.noreply.github.com>
2024-10-16 16:43:52 -0400

    Create 10_Wounds.py

---
+++ phb/pages/10_Wounds.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(page_title="Wounds", page_icon="phb/logo.png", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+st.markdown("<h1 style='text-align: center;'>Wounds</h1>", unsafe_allow_html=True)
+
+st.markdown("""
+<style>
+
+	.stTabs [data-baseweb="tab-list"] {
+		gap: 150px;
+    }
+
+</style>""", unsafe_allow_html=True)
+
+blunt, slashing, piercing, energy = st.tabs(["Blunt", "Slashing", "Piercing", "Energy"])
+
+st.markdown("<a href='#blunt'>Blunt</a>", unsafe_allow_html=True)
+st.markdown("<a href='#slashing'>Slashing</a>", unsafe_allow_html=True)
+st.markdown("<a href='#piercing'>Piercing</a>", unsafe_allow_html=True)
+st.markdown("<a href='#energy'>Energy</a>", unsafe_allow_html=True)
+
+wound = open("phb/md/Wounds.md", "r")
+st.write(wound.read())
