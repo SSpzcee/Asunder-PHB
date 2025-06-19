8985ee26aaac4701a38f23551aa4dab5c83e8d25
Machez360 <42299304+Machez360@users.noreply.github.com>
2025-06-19 10:54:24 -0500

    Update 4_Abilities.py

--- phb/pages/4_Abilities.py
+++ phb/pages/4_Abilities.py
@@ -4,14 +4,14 @@ st.set_page_config(page_title="Abilities", page_icon="phb/logo.png", layout="cen
 
 st.markdown("<h1 style='text-align: center;'>Abilities</h1>", unsafe_allow_html=True)
 
-st.markdown("<a href='#chakra-abilities'>Chakra Abilities</a>", unsafe_allow_html=True)
-st.markdown("<a href='#advanced-training'>Advanced Training</a>", unsafe_allow_html=True)
+st.markdown("<a href='#chakra-abilities'>Physical</a>", unsafe_allow_html=True)
+st.markdown("<a href='#chakra-abilities'>Chakra</a>", unsafe_allow_html=True)
 st.markdown("<a href='#genjutsu'>Genjutsu</a>", unsafe_allow_html=True)
-st.markdown("<a href='#physical'>Physical</a>", unsafe_allow_html=True)
-st.markdown("<a href='#sensory'>Sensory</a>", unsafe_allow_html=True)
-st.markdown("<a href='#subtlety'>Subtlety</a>", unsafe_allow_html=True)
+st.markdown("<a href='#advanced-training'>Sensory</a>", unsafe_allow_html=True)
+st.markdown("<a href='#physical'>Subtlety</a>", unsafe_allow_html=True)
+st.markdown("<a href='#sensory'>Supplementary</a>", unsafe_allow_html=True)
 st.markdown("<a href='#equipment'>Equipment</a>", unsafe_allow_html=True)
-st.markdown("<a href='#other'>Other</a>", unsafe_allow_html=True)
+st.markdown("<a href='#other'>Advanced Training</a>", unsafe_allow_html=True)
 st.markdown("<a href='#skill'>Skill</a>", unsafe_allow_html=True)
 st.markdown("<a href='#development'>Development</a>", unsafe_allow_html=True)
 
